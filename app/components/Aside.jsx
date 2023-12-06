71948de59cdf880f5632c171777069f23e1c0c30
YashviSoftpulse <softpulsedev15@gmail.com>
2023-12-06 18:03:44 +0530

    changes

--- app/components/Aside.jsx
+++ app/components/Aside.jsx
@@ -36,7 +36,7 @@ export function Aside({children, heading, id = 'aside'}) {
 
 function CloseAside() {
   return (
-    /* eslint-disable-next-line jsx-a11y/anchor-is-valid */
+   
     <a className="close" href="#" onChange={() => history.go(-1)}>
       &times;
     </a>
